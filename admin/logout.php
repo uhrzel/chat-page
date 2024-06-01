e9ec45405ff8766a9d9a348af13acbf7409aed8b
Arzel John Zolina <Arzeljrz17@gmail.com>
2024-06-01 23:55:19 +0800

    waw

--- admin/logout.php
+++ admin/logout.php
@@ -1,12 +1,10 @@
 <?php
 session_start();
 
-// Unset all of the session variables
+
 $_SESSION = array();
 
-// Destroy the session
 session_destroy();
 
-// Redirect to the login page or any other page as needed
 header("Location: ../admin.php");
 exit();
